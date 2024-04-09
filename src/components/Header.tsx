8e199f32841e8ff42e06fe7ed403632e6cffbf16
Pugialli <joaopugialli@gmail.com>
2024-04-09 13:54:31 -0300

    no theme yet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@
 // import { useTheme } from 'next-themes'
 import LogoEJCColorido from '@/assets/LogoEJCColorido'
 import Link from 'next/link'
-import { ModeToggle } from './theme/switch-theme-button'
 
 export function Header() {
   // const { resolvedTheme } = useTheme()
@@ -12,12 +11,12 @@ export function Header() {
   // const isDark = true
   return (
     <div className="flex w-full justify-between bg-violet-700 px-16 py-6">
-      <Link href="/participe">
+      <Link href="/">
         {/* <LogoEJC isDark={isDark} colored={true} /> */}
         <LogoEJCColorido />
       </Link>
 
-      <ModeToggle />
+      {/* <ModeToggle /> */}
     </div>
   )
 }

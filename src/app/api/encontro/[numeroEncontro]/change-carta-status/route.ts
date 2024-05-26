01f22702b75784fe1061379c039c19231f837aa7
Pugialli <joaopugialli@gmail.com>
2024-05-26 04:17:25 -0300

    update route to get

--- src/app/api/encontro/[numeroEncontro]/change-carta-status/route.ts
+++ src/app/api/encontro/[numeroEncontro]/change-carta-status/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { changeCartaStatus } from './change-carta-status'
 
-export async function PATCH() {
+export async function GET() {
   const status = await changeCartaStatus()
 
   return NextResponse.json(status)

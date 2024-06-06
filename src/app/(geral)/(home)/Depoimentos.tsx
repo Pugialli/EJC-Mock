c2b00f89fe582bd9c7914e89af74f721cce3fb8c
Pugialli <joaopugialli@gmail.com>
2024-06-06 17:16:26 -0300

    depoimentos att

--- src/app/(geral)/(home)/Depoimentos.tsx
+++ src/app/(geral)/(home)/Depoimentos.tsx
@@ -5,19 +5,19 @@ import { DepoimentoItem } from './(pageComponents)/DepoimentoItem'
 const depoimentosContent: ReactNode[] = [
   <DepoimentoItem
     key={0}
+    text="O EJC me trouxe amigos que vou levar para a vida inteira, além de experiências incríveis também! Sou muito grata ao movimento <3"
+    sourcePicture="https://res.cloudinary.com/ejc-nsdp/image/upload/v1713900085/people/225_iytfia.jpg"
+    name="Andressa Correa"
+    encontro="62"
+    since="2017"
+  />,
+  <DepoimentoItem
+    key={1}
     text="O EJC mudou minha vida porque eu conheci o Globar, e agora eu como pastel de nutella diariamente."
     sourcePicture=""
     name="Antônio Alves"
     encontro="55"
     since="2014"
-  />,
-  <DepoimentoItem
-    key={1}
-    text="O EJC me trouxe amigos que vou levar para a vida inteira, além de experiências incríveis também! Sou muito grata ao movimento <3"
-    sourcePicture="https://res.cloudinary.com/ejc-nsdp/image/upload/v1713900085/people/225_iytfia.jpg"
-    name="Andressa Correa"
-    encontro="62"
-    since="2016"
   />,
   <DepoimentoItem
     key={2}
@@ -36,6 +36,49 @@ const depoimentosContent: ReactNode[] = [
     since="2014"
   />,
 ]
+
+// const depoimentosContent: ReactNode[] = [
+//   <DepoimentoItem
+//     key={0}
+//     text="O EJC me trouxe amigos que vou levar para a vida inteira, além de experiências incríveis também! Sou muito grata ao movimento <3"
+//     sourcePicture="https://res.cloudinary.com/ejc-nsdp/image/upload/v1713900085/people/225_iytfia.jpg"
+//     name="Andressa Correa"
+//     encontro="62"
+//     since="2016"
+//   />,
+//   <DepoimentoItem
+//     key={1}
+//     text="O EJC mudou minha vida porque conheci pessoas incríveis que me aproximam de Deus e com quem posso dividir a vida fora da igreja também!"
+//     sourcePicture=""
+//     name="Luana Vidal"
+//     encontro="62"
+//     since="2016"
+//   />,
+//   <DepoimentoItem
+//     key={2}
+//     text="Ele foi o meu sorriso o meu amor e a minha vida!"
+//     sourcePicture=""
+//     name="Ana Margaret Pereira"
+//     encontro="2"
+//     since="1986"
+//   />,
+//   <DepoimentoItem
+//     key={3}
+//     text="O EJC mudou a minha vida, pois trouxe a minha fé de volta e me deu uma linda família! Esse movimento salva vidas."
+//     sourcePicture="https://res.cloudinary.com/ejc-nsdp/image/upload/v1713900074/people/314_tsbp7a.jpg"
+//     name="Lucas Zirretta"
+//     encontro="66"
+//     since="2019"
+//   />,
+//   <DepoimentoItem
+//     key={4}
+//     text="O EJC fez mudar a minha perspectiva de vida! Fora que pude conhecer pessoas maravilhosas das quais, hoje em dia, não consigo viver sem!"
+//     sourcePicture=""
+//     name="Bárbara Paiva"
+//     encontro="62"
+//     since="2017"
+//   />,
+// ]
 
 export function Depoimentos() {
   return (

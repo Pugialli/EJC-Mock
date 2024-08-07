6e04b9c7be3ed75870cff2a5afb13f9d8866083b
Pugialli <joaopugialli@gmail.com>
2024-08-07 16:20:28 -0300

    72 ejc

--- src/app/(geral)/participe/page.tsx
+++ src/app/(geral)/participe/page.tsx
@@ -1,23 +1,29 @@
-// import type { EncontroData } from '@/app/api/encontro/[numeroEncontro]/get-encontro'
+import type { EncontroData } from '@/app/api/encontro/[numeroEncontro]/get-encontro'
 import { CreateEncontristaContextProvider } from '@/context/CreateEncontristaContext'
 import { ParticipeForm } from './(form)/ParticipeForm'
 
-// async function fetchDataEncontro() {
-//   const response = await fetch(`${process.env.NEXTAUTH_URL}/api/encontro/71`)
-//   if (response.ok) {
-//     const data: EncontroData = await response.json()
-//     return new Date(data.dataInicio)
-//   }
+async function fetchDataEncontro() {
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/encontro/72`)
+  if (response.ok) {
+    const data: EncontroData = await response.json()
+    const date = new Date(data.dataInicio.split('T')[0])
+    const fixedDate = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate() + 1,
+    )
+    return fixedDate
+  }
 
-//   return null
-// }
+  return null
+}
 
 export default async function Participe() {
-  // const dataEncontro: Date | null = await fetchDataEncontro()
+  const dataEncontro: Date | null = await fetchDataEncontro()
 
   return (
     <CreateEncontristaContextProvider>
-      <ParticipeForm dataInicio={null} />
+      <ParticipeForm dataInicio={dataEncontro} />
     </CreateEncontristaContextProvider>
   )
 }

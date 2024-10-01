65debb7a5736abbdbb8b1c221754364ee15ba056
Pugialli <joaopugialli@gmail.com>
2024-10-01 11:22:03 -0300

    fix updateEndereco

--- src/app/api/endereco/[cep]/update/update-endereco.ts
+++ src/app/api/endereco/[cep]/update/update-endereco.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { getCEPData, type CEPResponse } from '@/utils/fetch-cep'
 
 export interface EnderecoUpdateProps {
   cep: string
@@ -11,6 +12,31 @@ export async function updateEndereco({
   bairro,
   rua,
 }: EnderecoUpdateProps) {
+  const foundEndereco = await prisma.endereco.findUnique({
+    where: {
+      cep,
+    },
+  })
+  if (!foundEndereco) {
+    const response = await getCEPData(cep)
+
+    if (response === undefined) {
+      return null
+    }
+
+    const addressData: CEPResponse = await response.json()
+
+    return await prisma.endereco.create({
+      data: {
+        cep,
+        bairro,
+        rua,
+        cidade: addressData.city,
+        estado: addressData.state,
+      },
+    })
+  }
+
   return await prisma.endereco.update({
     data: {
       bairro,
